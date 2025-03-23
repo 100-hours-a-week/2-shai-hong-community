2920441b650bf329c12b8bd7775a937bf967c79c
CoderDuck <hongdongk@naver.com>
2025-03-23 22:05:49 +0900

    feat: 로그인기능 백엔드 연동

--- js/login.js
+++ js/login.js
@@ -1,11 +1,12 @@
-import { checkemailPattern, checkpasswordPattern } from './function/validation.js';
+import { checkemailPattern, checkpasswordPattern } from "./util/validation.js";
+import { storeData } from "./util/storedata.js";
 
-const loginForm = document.getElementById('loginForm');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-const loginButton = document.getElementById('loginButton');
-const emailError = document.getElementById('error1');
-const passwordError = document.getElementById('error2');
+const loginForm = document.getElementById("loginForm");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("loginButton");
+const emailError = document.getElementById("error1");
+const passwordError = document.getElementById("error2");
 
 let emailCheck = false;
 let passwordCheck = false;
@@ -14,9 +15,9 @@ function validateEmail() {
   emailCheck = checkemailPattern(emailInput.value);
 
   if (emailCheck) {
-    emailError.classList.remove('error-on');
+    emailError.classList.remove("error-on");
   } else {
-    emailError.classList.add('error-on');
+    emailError.classList.add("error-on");
   }
 
   updateLoginButton();
@@ -26,9 +27,9 @@ function validatePassword() {
   passwordCheck = checkpasswordPattern(passwordInput.value);
 
   if (passwordCheck) {
-    passwordError.classList.remove('error-on');
+    passwordError.classList.remove("error-on");
   } else {
-    passwordError.classList.add('error-on');
+    passwordError.classList.add("error-on");
   }
 
   updateLoginButton();
@@ -37,21 +38,43 @@ function validatePassword() {
 // 로그인버튼 활성화버튼
 function updateLoginButton() {
   if (emailCheck && passwordCheck) {
-    loginButton.classList.add('active');
-    loginButton.removeAttribute('disabled');
+    loginButton.classList.add("active");
+    loginButton.removeAttribute("disabled");
   } else {
-    loginButton.classList.remove('active');
-    loginButton.setAttribute('disabled', 'true');
+    loginButton.classList.remove("active");
+    loginButton.setAttribute("disabled", "true");
   }
 }
 
-emailInput.addEventListener('input', validateEmail);
-passwordInput.addEventListener('input', validatePassword);
+const loginfetch = async (loginData) => {
+  try {
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: loginData,
+    });
 
-loginForm.addEventListener('submit', function (event) {
+    const result = await response.json();
+    storeData("user", result);
+    window.location.replace("../postlist.html");
+  } catch (err) {
+    console.log(err);
+    window.alert("로그인 실패");
+  }
+};
+
+emailInput.addEventListener("input", validateEmail);
+passwordInput.addEventListener("input", validatePassword);
+
+loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
   if (emailCheck && passwordCheck) {
-    window.alert('로그인 성공');
-    window.location.replace('../postlist.html');
+    let loginData = JSON.stringify({
+      email: `${emailInput.value}`,
+      password: `${passwordInput.value}`,
+    });
+    loginfetch(loginData);
   }
 });
